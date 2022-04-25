b36b2aed64a4d9a28f29c45cd3bae0019a66002b
smnzk <104374966+smnzk@users.noreply.github.com>
2022-04-25 20:42:07 +0200

    Delete API2 directory

--- API2/API2/Models/Medicament.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PrzykladKolokwium.Models
-{
-    public class Medicament
-    {
-        public int idMedicament { get; set; }
-        public int dose { get; set; }
-        public string details { get; set; }  
-    }
-}
